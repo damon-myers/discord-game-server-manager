76010588b6b511b3231fe1cf5ebb22c28fc01108
Damon Myers <damon.shane.myers@gmail.com>
2021-02-24 19:02:42 -0800

    refactor: Remove unused Lambda code

--- src/handlers/hello.ts
+++
@@ -1,6 +0,0 @@
-import { APIGatewayProxyResult } from 'aws-lambda';
-
-export const handler = async (): Promise<APIGatewayProxyResult> => {
-  console.log(`time to say 'hello'`);
-  return { body: JSON.stringify({ message: 'hello' }), statusCode: 200 };
-};
