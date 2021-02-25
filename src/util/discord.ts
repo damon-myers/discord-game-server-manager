7c1eedfd3d5cfb3e1cc7f5720cae0220c7661c23
Damon Myers <damon.shane.myers@gmail.com>
2021-02-24 19:37:58 -0800

    fix: Handle signature verification succesfully

--- src/util/discord.ts
+++ src/util/discord.ts
@@ -2,15 +2,26 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import * as nacl from 'tweetnacl';
 
 export function isValidRequestSignature(request: APIGatewayProxyEvent, appPublicKey: string): boolean {
-  const signature = request.headers['X-Signature-Ed25519'];
-  const timestamp = request.headers['X-Signature-Timestamp'];
+  const signature = request.headers['x-signature-ed25519'];
+  const timestamp = request.headers['x-signature-timestamp'];
   const body = request.body;
 
   console.log(`Got:\nsignature:${signature},\ntimestamp: ${timestamp},\nbody: ${body}`)
 
-  return signature && timestamp && nacl.sign.detached.verify(
-    Buffer.from(timestamp + body),
-    Buffer.from(signature, 'hex'),
-    Buffer.from(appPublicKey, 'hex')
-  );
+  if (!signature || !timestamp) {
+    return false;
+  }
+
+  let isValid = false;
+  try {
+    isValid = nacl.sign.detached.verify(
+      Buffer.from(timestamp + body),
+      Buffer.from(signature, 'hex'),
+      Buffer.from(appPublicKey, 'hex')
+    );
+  } catch (_) {
+    return false;
+  }
+
+  return isValid;
 }
